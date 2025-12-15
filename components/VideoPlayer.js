bb7256df5f310974b581ecc21949b301cebc5b98
Haniyeh Afrasiyabi <afrasiyabihanie@gmail.com>
2025-12-15 20:19:21 +0330

    feat: add basic MKV streaming video player using Shaka

--- components/VideoPlayer.js
+++ components/VideoPlayer.js
@@ -1,5 +1,29 @@
-function VideoPlayer() {
-  return <div>VideoPlayer</div>;
+"use client";
+
+import { useEffect, useRef } from "react";
+import shaka from "shaka-player/dist/shaka-player.compiled";
+
+function VideoPlayer({ src }) {
+  const videoRef = useRef(null);
+  const playerRef = useRef(null);
+
+  useEffect(() => {
+    shaka.polyfill.installAll();
+
+    if (!shaka.Player.isBrowserSupported()) {
+      alert("Browser not supported");
+      return;
+    }
+
+    const player = new shaka.Player(videoRef.current);
+    playerRef.current = player;
+
+    player.load(src).catch(console.error);
+
+    return () => player.destroy();
+  }, [src]);
+
+  return <video ref={videoRef} controls className="w-full bg-black rounded" />;
 }
 
 export default VideoPlayer;
